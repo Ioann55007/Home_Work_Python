903e7377736289cf92164f27966c3356d4114a99
ioann <ioann.basic@gmail.com>
2023-02-04 18:49:11 +0500

    Задание к 25-ой паре.

--- one_home.py
+++ one_home.py
@@ -1,8 +1,52 @@
+from math import pi
+
+# Вычисление площадей
+
+# ax = lambda x, f: x * (f ** 2)
+# print(ax(pi, 2))
+#
+# ac = lambda x, f: x * f
+# print(ac(10, 13))
+#
+# af = lambda x, f, j: (x + f) * j / 2
+# print(af(7, 5, 3))
+
+# Произведение трёх чисел
+
+# df = lambda a, v, m: a * v * m
+# print(df(2, 5, 5))
 
 
-x = 'Hi I'
+# test = [{'name': 'Jenifer', 'final': 95},
+#         {'name': 'David', 'final': 92},
+#         {'name': 'Nicolas', 'final': 98}]
 
 
+# сортировка
+# db_sorted = sorted(test, key=lambda a: a['name'])
+# db_sort = sorted(test, key=lambda r: r['final'], reverse=True)
+#
+# print(db_sorted)
+# print(db_sort)
+
+# нахождение минимального, максимального значения.
+# print(min(test, key=lambda a: a['final']))
+# print(max(test, key=lambda s: s['final']))
+
+
+# преобразование последовательности.
+
+
+# g = [3, 6, 8, 9, 1, 2]
+#
+# print(list(map(lambda x: x * g.index(x) ** 3, g)))
+
+
+# возведение в квадрат и куб элементов списка
+
+# a = [3, 5, 7, 3, 9, 5, 7, 2]
+# print(list(map(lambda x: x ** 2, a)))
+# print(list(map(lambda x: x ** 3, a)))
 
 
 
