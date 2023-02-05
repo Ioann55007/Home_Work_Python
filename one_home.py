d27d8e5f24f48d8d6e07a8f3bb4e5fb7ff089cb1
ioann <ioann.basic@gmail.com>
2023-02-05 22:39:30 +0500

    Задание к 30-ой паре.

--- one_home.py
+++ one_home.py
@@ -1,23 +1,56 @@
-def decor(munk):
-    def drapiz(*args):
-        print(munk(*args) / len(args))
+# Удаление первого и последнего вхождения.
+# a_str = 'I am learning Python. hello, WORLD!'
+# r_str = a_str[:a_str.find('h')]
+# f_str = a_str[1 + a_str.rfind('h'):]
+# print(r_str)
+# print(f_str)
+#
+# df = r_str + f_str
+# print(df)
+import re
 
-    return drapiz
+# Разворот последовательности символов между первым и последним вхождением символа "h" в противоположном порядке.
+# ztr = 'I am learning Python. hello, WORLD!'
+# r_str = ztr[:ztr.find('h') + 1]
+# f_str = ztr[1 + ztr.find('h'):]
+# jraf = f_str[:f_str.find('h')]
+# tre = ''.join(reversed(jraf))
+# ar = tre.lstrip()
+# fraj = f_str[f_str.find('h'):]
+# concatin = ar + fraj
+# ra = ' ' + concatin
+# con_concatin = r_str + ra
+# print(r_str)
+# print(ar)
+# print(fraj)
+# print(concatin)
+# print(ra)
+# print(con_concatin)
 
 
+# Замена подстроки.
 
-@decor
-def fdevv(*args):
-    print(sum(args))
-    return sum(args)
+# n = input("Строка: ")
+#
+# print(n)
+# i = input("Заменяемая подстрока: ")
+# print(i)
+# je = input('Новая подстрока: ')
+# drf = n.replace(i, je)
+# print('Конечный результат: ',  drf)
 
 
-fdevv(1, 15, 23)
-
-
-
-
-
+# Найти колличество слов, начинающихся с буквы "е".
+#
+# a_str = 'Ежевику для ежат  еле-еле принесли два ежа'
+# lk = a_str.split(" ")
+# i_str = 'е'
+# l = ''
+#
+# for k in lk:
+#     if k.startswith(i_str):
+#         o = l.join(k)
+#         print(o)
 
 
 
