cd8be8415b3205b471d84968d30aa1b48d7e9785
ioann <ioann.basic@gmail.com>
2023-02-14 00:22:00 +0500

    Рекурсия

--- one_home.py
+++ one_home.py
@@ -1,16 +1,15 @@
-import re
 
-my_str = r'\+7\s\d{3}\s\d{3}\-\d{2}\-\d{2}'
-my_str_1 = r'\+7\d{10}'
-my_str_2 = r'7\s\(\d{3}\)\s\d{3}\s\d{2}\s\d{2}'
-my_str_3 = r'7\s\(\d{3}\)\s\d{3}\-\d{2}\-\d{2}'
-
-pr_int = input('Введите номер телефона: ')
-ccb = re.findall(my_str_3, pr_int)
-bbc = re.findall(my_str_2, pr_int)
-oi = re.findall(my_str_1, pr_int)
-cv = re.findall(my_str, pr_int)
-if oi or cv or bbc or ccb:
-    print('IS ok!')
-else:
-    print('False')
+# def f_func(n):
+#     if len(n) == 0:
+#         return 0
+#     else:
+#         count_er = f_func(n[1:])
+#
+#         if n[0] < 0:
+#             count_er += 1
+#         return count_er
+#
+#
+# v = [-2, 3, 8, -11, -4, 6, -18]
+#
+# print(f_func(v))
