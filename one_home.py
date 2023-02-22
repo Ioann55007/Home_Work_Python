eb22d989178be5a4543b64793d4ed8a2a1b71f67
ioann <ioann.basic@gmail.com>
2023-02-22 13:44:55 +0500

    Гачало с ООП

--- one_home.py
+++ one_home.py
@@ -1,40 +1,34 @@
-with open('my_homework_file.txt', 'w+') as home_file:
-    home_file.write('Заменить строки в текстовом файле; \nизменить строку в списке; \nзаписать список в файл')
+class Car:
+    def __init__(self, name_model, year_of_issue, manufacturer, power_engine, colored_car, price):
+        self.name_model = name_model
+        self.year_of_issue = year_of_issue
+        self.manufacturer = manufacturer
+        self.power_engine = power_engine
+        self.colored_car = colored_car
+        self.price = price
 
-hom_fil = open('my_homework_file.txt', 'r')
-read_file = hom_fil.readlines()
-print(read_file)
-x_file = read_file[0]
-read_file[0] = read_file[1]
-read_file[1] = x_file
-print(read_file)
-read_file[0] = 'Изменить строку толь ко одну'
-print(read_file)
-last_file = open('my_homework_file.txt', 'w')
-last_file.writelines(read_file)
-last_file.close()
+    def set_car(self):
+        print('Установите: ', self.name_model, self.year_of_issue, self.manufacturer, self.power_engine,
+              self.colored_car,
+              self.price)
 
+    def get_data(self):
+        data_car = (
+            'Автомобиль марки - ', self.name_model, 'года выпуска -', self.year_of_issue, 'производителя - ',
+            self.manufacturer,
+            'мощностью двигателя - ', self.power_engine, 'цвета - ', self.colored_car, 'стоимость - ', self.price)
 
-with open('my_homework.txt', 'w+') as home_file:
-    home_file.write('Заменить строки в текстовом файле; \nизменить строку в списке; \nзаписать список в файл')
-
-f = open('my_homework.txt', 'r')
-reading_file = f.readlines()
-print(reading_file)
-
-reading_file.reverse()
-print(reading_file)
+        return data_car
 
 
-a_d = 'my_homework_file.txt'
-d_a = 'my_homework.txt'
+new_my_car = Car('БМВ,', '2017,', 'Германия,', '2100 лошадинных сил,', 'чёрного,', '10000 $')
+new_my_car.set_car()
+car_nw = new_my_car.get_data()
+print(car_nw)
+g = new_my_car.colored_car
+r = new_my_car.power_engine
+rg = new_my_car.price
+print(g, r, rg)
 
-with open(a_d, 'r') as f, open(d_a, 'r') as fi:
-    new = f.read(), fi.read()
-    print(new)
 
-print(new)
-fo = open('threefile.txt', 'w+')
-fo.writelines(new)
-fo.close()
 
