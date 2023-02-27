b17b2d68a905ecfefab4d2eb4af85ab38fbea29b
ioann <ioann.basic@gmail.com>
2023-02-28 00:05:49 +0500

    Реализация класса Сфера

--- one_home.py
+++ one_home.py
@@ -1,34 +1,52 @@
-class Car:
-    def __init__(self, name_model, year_of_issue, manufacturer, power_engine, colored_car, price):
-        self.name_model = name_model
-        self.year_of_issue = year_of_issue
-        self.manufacturer = manufacturer
-        self.power_engine = power_engine
-        self.colored_car = colored_car
-        self.price = price
-
-    def set_car(self):
-        print('Установите: ', self.name_model, self.year_of_issue, self.manufacturer, self.power_engine,
-              self.colored_car,
-              self.price)
-
-    def get_data(self):
-        data_car = (
-            'Автомобиль марки - ', self.name_model, 'года выпуска -', self.year_of_issue, 'производителя - ',
-            self.manufacturer,
-            'мощностью двигателя - ', self.power_engine, 'цвета - ', self.colored_car, 'стоимость - ', self.price)
-
-        return data_car
+import math
+from math import pi
 
 
-new_my_car = Car('БМВ,', '2017,', 'Германия,', '2100 лошадинных сил,', 'чёрного,', '10000 $')
-new_my_car.set_car()
-car_nw = new_my_car.get_data()
-print(car_nw)
-g = new_my_car.colored_car
-r = new_my_car.power_engine
-rg = new_my_car.price
-print(g, r, rg)
+class Sphere:
 
+    def __init__(self, get_square, set_radius, get_volume, set_center, get_center, get_radius, is_point_inside):
+        self.get_volume = get_volume
+        self.get_square = get_square
+        self.get_radius = get_radius
+        self.get_center = get_center
+        self.set_radius = set_radius
+        self.get_radius = get_radius
+        self.set_center = set_center
+        self.is_point_inside = is_point_inside
+
+
+    @property
+    def get_sphere(self):
+        self.get_square = 4 * pi * self.set_radius ** 2
+        self.get_volume = 4 / 3 * (pi * self.set_radius ** 3)
+        get_square = ('Выводим площадь сферы: ', self.get_square)
+        get_volume = ('Выводим обьём шара: ', self.get_volume)
+        self.get_center = self.set_center
+        get_center = ("Выводим центр сферы: ", self.get_center)
+        self.get_radius = self.set_radius
+        get_radius = ('Выводим радиус сферы ', self.get_radius)
+
+        return get_square, get_volume, get_center, get_radius
+
+    @get_sphere.setter
+    def get_sphere(self, value):
+        self.set_radius = value
+
+        self.set_center = value
+
+    def point_inside(self, x, y, z):
+
+        if math.sqrt((x - x) ** 2 + (y - y) ** 2 + (z - z) ** 2) <= self.set_radius:
+
+            return True
+        else:
+            return False
+
+
+t = 12, 32, 11
+oz = Sphere(None, 18, None, t, None, None, None)
+print(oz.get_sphere)
+print(oz.point_inside(12, 13, 444))
+print('Радиус сферы установлен в значении: ', oz.set_radius)
 
 
