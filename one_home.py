427d11f31b73ac881d806f4f8dcf632e279b5795
ioann <ioann.basic@gmail.com>
2023-03-07 21:56:18 +0500

    Наcледование 2 урок

--- one_home.py
+++ one_home.py
@@ -1,94 +1,26 @@
-class Account:
-    rate_us = 0.013
-    rate_eur = 0.011
-    sufix = 'RUB'
-    sufix_usd = 'USD'
-    sufix_er = 'EUR'
-    commission = 5
+class Student:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.k = self.PropertyLaptop()
+
+    def cal_student(self):
+        print(f'Ноутбук был подарен студентам {self.a, self.b}, с характеристиками: {self.k.show()}')
 
 
-    def __init__(self, num, surname, percent, value=0):
-        self.num = num
-        self.surname = surname
-        self.percent = percent
-        self.value = value
-        print(f"Счёт #{self.num} принадлежащий {self.surname} был открыт")
-        print("*" * 50)
+    class PropertyLaptop:
+        def __init__(self):
+            self.model = 'HP'
+            self.proc = 'i7'
+            self.memory = '16GB'
 
-    def __del__(self):
-        print(f'Счёт владельца {self.surname} закрыт')
+        def show(self):
 
-    @classmethod
-    def set_usd_rate(cls, rate):
-        cls.rate_usd = rate
-
-    @staticmethod
-    def convert(value, rate):
-        return value * rate
-
-    @staticmethod
-    def give_commission():
-        return Account.commission
+            message = f"модель ноутбука {self.model}, процессор {self.proc}, и память {self.memory}"
+            return message
 
 
-    def convert_to_usd(self):
-        usd_val = Account.convert(self.value, Account.rate_us)
-        usd_vale = Account.convert(self.value, Account.rate_eur)
-        print(f"Состояние счёта: {usd_val}{Account.sufix}.")
-        print(f"Состояние счёта: {usd_vale}{Account.sufix_usd}.")
-
-    def print_balanc(self):
-        print(f'Текущий баланс {self.value}{Account.sufix}')
-
-    def print_info(self):
-        print('Инфа о счёте')
-        print(f"#{self.num}")
-        print(f"Владелец: {self.surname}")
-        self.print_balanc()
-        print(f'Процент: {self.percent:.0%}')
-        print('-' * 20)
-
-    def edit_owner(self, surname):
-        self.surname = surname
-
-    def add_percent(self):
-        self.value += self.value * self.percent
-        print('Проценты начислены!')
-        self.print_balanc()
-
-
-    def with_drow_many(self, val):
-        if val > self.value:
-            print(f"К сожалению у вас нет заданной суммы  денег {val}{Account.sufix}")
-
-
-        else:
-            print(f'Ваша коммиссия: {self.give_commission()}%')
-            print_commission = (self.value / 100) * self.give_commission()
-            val -= print_commission
-            self.value -= val
-            print(f"{val}{Account.sufix} было успешно снято!")
-        self.print_balanc()
-
-    def add_many(self, val):
-        self.value += val
-        print(f"{val}{Account.sufix} было успешно добавлено!")
-        self.print_balanc()
-
-
-account = Account('12345', 'Долгих', 0.03, 1000)
-account.print_info()
-account.convert_to_usd()
-account.edit_owner('Дима')
-account.print_info()
-account.add_percent()
-print()
-account.with_drow_many(100)
-account.add_many(5000)
-account.with_drow_many(2000)
-
-
-
-
-
-
+show_answear = Student('Роману', 'Владимиру')
+show_answear.cal_student()
+s = show_answear.PropertyLaptop()
+s.show()
